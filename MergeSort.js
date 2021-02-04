15f50565997991dc5926c652463f5216e42ece0a
Sivarajan Sivanesan <42610564+sivarajans@users.noreply.github.com>
2021-02-04 13:13:08 +0530

    Update MergeSort.js

--- MergeSort.js
+++ MergeSort.js
@@ -1,14 +1,14 @@
 function mergeSort(a, lb, ub) {
-   if (lb < ub) {
+   if (lb < ub) { //stop when index are equal.
        let mid = Math.floor((lb + ub) / 2);
        mergeSort(a, lb, mid);
        mergeSort(a, mid+1, ub);   
-       divideConquer(a, lb, mid, ub);
+       divideAndConquer(a, lb, mid, ub);
    }  
 
 }
 
-function divideConquer(a, lb, mid, ub){
+function divideAndConquer(a, lb, mid, ub){
   let i = lb;
   let j = mid + 1;
   let k = lb;
@@ -27,16 +27,20 @@ function divideConquer(a, lb, mid, ub){
    k++;
  }
 
+ // remaining left side elements
  while ( i <= mid ) { 
    sorted[k] = a[i];
    i++; k++;
  }
+   
+ //remaining right side elements
  while (j <= ub)
  { 
    sorted[k] = a[j];
    j++; k++;
  }
- console.log(sorted);
+
+ // copy to original array
  for(k=lb; k <= ub; k++)
    a[k] = sorted[k];
 }

b3512a55ee9e6ade6c96f9e1f63d02c28fb2207f
Sivarajan Sivanesan <42610564+sivarajans@users.noreply.github.com>
2021-02-11 20:46:55 +0530

    Update Quick Sort.js

--- Quick Sort.js
+++ Quick Sort.js
@@ -1,3 +1,19 @@
+/*
+Quick Sort - sorts the array using the pivot position.
+we can choose any one value as pivot, here I have chosen first element as pivot.
+*/
+function QuickSort(a, lb, ub) {
+  if (lb < ub) {
+    
+    // Returns the pivot position index that is moved to right position
+    let pivotpos = quickMove(a, lb, ub);
+    
+    // Now move other elements in array other than pivot position recursively.
+    QuickSort(a, lb, pivotpos -1);
+    QuickSort(a, pivotpos + 1, ub)
+  }
+}
+
 function quickMove(a, lb, ub) {
   let start = lb;
   let end = ub;
@@ -23,15 +39,12 @@ function swap(a, i , j) {
  a[j] = temp;
 }
 
-function quickSort(a, lb, ub) {
-  if (lb < ub) {
-    let pivotpos = quickMove(a, lb, ub);
-    quickSort(a, lb, pivotpos -1);
-    quickSort(a, pivotpos + 1, ub)
-  }
-}
+
 
 
 //testing
-let arg = [10, 2, 20, 5, 1, 2001, 3]
-quickSort(arg, 0 , arg.length - 1);
+let arg = [10, 2, 20, 5, 1, 2001, 3];
+console.log('given array', arg);
+
+QuickSort(arg, 0 , arg.length - 1);
+console.log('Quick sorted', arg);

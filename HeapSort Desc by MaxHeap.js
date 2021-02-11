7d8b6e5c38220f998ecf2ab275cca509afe78599
Sivarajan Sivanesan <42610564+sivarajans@users.noreply.github.com>
2021-02-11 16:15:07 +0530

    Update HeapSort Desc by MaxHeap.js

--- HeapSort Desc by MaxHeap.js
+++ HeapSort Desc by MaxHeap.js
@@ -1,5 +1,18 @@
+/*
+Heap Sort - Descending Order Heap is used - Max Heap Invarient data structure.
 
-// does heap sorting
+1. Do max heap invarient structure.
+2. Swap last element with root 
+3. Pop last element (which is root) that has maximum value.
+
+Zero Based Index Formula:
+-----------------------------
+All the parent = floor(n / 2) - 1 -- here n is total elements.
+
+Child-1 of parent = 2i + 1 -- here i is index (0 based)
+Child-2 of parent = 2i + 2
+*/
+
 function HeapSort(a) {
     let sorted = [];
 

54b06737c6f72d19e1126a76ae395c94a22b46c7
Sivarajan Sivanesan <42610564+sivarajans@users.noreply.github.com>
2021-02-11 16:13:46 +0530

    Update HeapSort Asc by MinHeap.js

--- HeapSort Asc by MinHeap.js
+++ HeapSort Asc by MinHeap.js
@@ -1,3 +1,19 @@
+/*
+Heap Sort - Ascending Order Heap is used - Min Heap Invarient data structure.
+
+1. Do min heap invarient structure.
+2. Swap last element with root 
+3. Pop last element (which is root) that has minimum value.
+
+Zero Based Index Formula:
+-----------------------------
+All the parent = floor(n / 2) - 1 -- here n is total elements.
+
+Child-1 of parent = 2i + 1 -- here i is index (0 based)
+Child-2 of parent = 2i + 2
+*/
+
+
 function HeapSort(a) {
     let sorted = [];
 

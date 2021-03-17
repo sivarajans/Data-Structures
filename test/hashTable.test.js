fac7f6922a3452792a88ba9cb65376b5a8a40879
Sivarajan Sivanesan <sivarajan.nesan@gmail.com>
2021-03-17 19:09:50 +0530

    changes for packages

--- test/hashTable.test.js
+++ test/hashTable.test.js
@@ -1,21 +1,32 @@
-// const HashTable = require('../lib/Hash Table/Hash Table').default;
+const HashTable = require('../lib/Hash Table/Hash Table').default;
+var assert = require('assert');
 
 
-// let hashtable = new HashTable(10);
+describe("Hash Table", () => {
+    it("verify hashtable", () => {
+        let hashtable = new HashTable(10);
+        hashtable.add(5, { mike: 5 });
+        hashtable.add(1, { one: 1 });
+        hashtable.add('5', { sttringFiv: 1 });
+        hashtable.add('x1231', { len5: true });
+        hashtable.add('who', { len3: true });
 
-// hashtable.add(5, { mike: 5 });
-// hashtable.add(1, { one: 1 });
-// hashtable.add('5', { sttringFiv: 1 });
-// hashtable.add('x1231', { len5: true });
-// hashtable.add('who', { len3: true });
+        hashtable.remove('5');
+        hashtable.remove('12345');
+        hashtable.remove(1);
+        let one = hashtable.find(1);
+        let three = hashtable.find(3);
+        let fiveletter = hashtable.find('x1231');
+        let who = hashtable.find('who');
+        let unknown = hashtable.find('23d');
 
-// console.log(hashtable.elements);
+        assert.strictEqual(one, null);
+        assert.strictEqual(three, null);
+        assert.strictEqual(fiveletter.value.len5, true);
+        assert.strictEqual(unknown, null);
+        assert.strictEqual(who.value.len3, true);
+    });
+});
 
-// hashtable.remove('5');
-// hashtable.remove('12345');
-// hashtable.remove(1);
-// let one = hashtable.find(1);
-// let three = hashtable.find(3);
-// let five = hashtable.find('x1231');
-// let unknown = hashtable.find('23d');
+
 
